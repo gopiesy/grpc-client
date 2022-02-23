f2442ac4098d7a1222608feeae3396823268f044
GopiChand Komma <gkomma@apple.com>
2022-02-22 19:56:47 -0600

    initial commit

---
+++ Makefile
@@ -0,0 +1,13 @@
+GO_PKG_DIRS  := $(subst $(shell go list -e -m),.,$(shell go list ./ | grep -v /vendor/ ))
+
+all:
+	go build -ldflags="-s -w" -o client $(GO_PKG_DIRS)
+
+fmt:
+	gofmt -s -w $(GO_PKG_DIRS)
+
+lint:
+	golangci-lint run -v $(GO_PKG_DIRS)
+
+clean:
+	rm -f client
---
+++ client.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"flag"
+
+	"io"
+	"log"
+
+	"github.com/gopiesy/project-protos/policies"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	serverAddr = flag.String("server_addr", "localhost:9111", "The server address in the format of host:port")
+	count      = flag.Int("count", 5, "Number of snapshots")
+)
+
+func runExecute(client policies.PolicyServiceClient) {
+	ctx := context.Background()
+
+	stream, err := client.StreamSnapshots(ctx)
+	if err != nil {
+		log.Fatalf("%v.StreamSnapshots(ctx) = %v, %v: ", client, stream, err)
+	}
+
+	for {
+		if *count != 0 {
+			snapshot, err := stream.Recv()
+			if err != nil {
+				if err == io.EOF {
+					log.Println("EOF reached exiting Client")
+					return
+				}
+				log.Fatalf("Err in Recv: %v", err)
+			}
+
+			log.Println("Snapshot received: ", snapshot.Name)
+			*count -= 1
+
+			// send response
+			status := policies.SnapshotStatus{SnapshotName: snapshot.Name}
+			if err := stream.Send(&status); err != nil {
+				log.Fatalf("Err in Send: %v", err)
+			}
+		}
+		log.Println("Bye Bye server")
+		if err := stream.CloseSend(); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(opts, grpc.WithBlock())
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+	client := policies.NewPolicyServiceClient(conn)
+
+	runExecute(client)
+}
